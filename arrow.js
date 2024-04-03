9c4638c0339f48934c3742fdfe584e09a99514c5
sagar30 <sagarpatil30720@gmail.com>
2024-04-03 12:16:44 +0530

    arrowfunction

---
+++ arrow.js
@@ -0,0 +1,12 @@
+// arrow functions
+// syntax
+
+// const myFunction = (param1, param2) => {
+//     // Function body
+//   };
+
+const text = (test1, test2) => test1 + test2;
+console.log(text(100,300));
+
+let myFunction = (a, b) => a * b;
+console.log(myFunction(2, 3));
