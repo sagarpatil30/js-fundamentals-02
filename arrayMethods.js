1d8fa5876e62038541282e99317e6eec2bb7aee8
sagar30 <sagarpatil30720@gmail.com>
2024-04-03 15:08:58 +0530

    array_methods

---
+++ arrayMethods.js
@@ -0,0 +1,23 @@
+// methods in array
+
+// push
+// adds the value at last
+const team =['cat','dog','monkey']
+team.push('rat');
+console.log(team);
+
+// pop
+// removes the value from last
+const players =['player1','player2','player3'];
+players.pop(2)
+console.log(players);
+
+// unshift adds the value in the first
+const numbers =['1','2','3']
+numbers.unshift('0')
+console.log(numbers);
+
+// Shift removes the value from first
+const fruits=['apple','sweetpotato']
+fruits.shift('apple')
+console.log(fruits);
