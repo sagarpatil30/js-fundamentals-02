f7fa8e428875c07a047477428c8fa0a0fbb8bc25
sagar30 <sagarpatil30720@gmail.com>
2024-04-03 17:44:21 +0530

    forloop

--- forloop.js
+++ forloop.js
@@ -1,3 +1,10 @@
 for (let count = 1; count <= 10; count++) {
   console.log(`hello javascript ${count}`);
 }
+
+
+const teams = ["rcb", "csk", "lsg", "mi", "kxip"];
+for (let i = 0; i < teams.length; i++) {
+  console.log(`my fav team is - ${teams[i]}`);
+}
+
