de66b0a40b876bc3bf6b8f99f6067f8d59761f96
sagar30 <sagarpatil30720@gmail.com>
2024-04-03 13:05:18 +0530

    arrays

---
+++ array.js
@@ -0,0 +1,16 @@
+// arrays in js
+const games =['cricket','football','tennis','kickBoxing']
+console.log(games);
+console.log(games[2]);
+console.log(games.length);
+console.log(games[games.length-3]);
+
+// upserting the new value in array
+games[2] = 'hockey';
+console.log(games);
+
+const years = new Array(100,200,300,400)
+console.log(years);
+
+const details = ['tester','developer',101,games,20+30]
+console.log(details);
