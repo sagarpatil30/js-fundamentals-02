817a2bc847bc749504e0d70d52d3fa25561ef734
sagar30 <sagarpatil30720@gmail.com>
2024-04-03 15:32:05 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,14 @@
+// objects
+// objects in js are key value pair
+
+const team = {
+  batsmen: "vk",
+  bowler: "pk",
+  wicketkeeper: "dk",
+  teamname: "losers",
+  years: 2024 - 2008,
+  yearoffinals:[2009,2011,2016]
+};
+
+console.log(team.yearoffinals[1]);
+console.log(team.yearoffinals);
